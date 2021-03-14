3de9aa9e929a66a7f1cab7ba3132bb1dae7363fa
Sebastian Weszler <s.weszler@gmail.com>
2021-03-14 23:12:52 +0800

    Wandering Robot - optimal

--- 2020_B/B4/b4.py
+++ 2020_B/B4/b4.py
@@ -68,14 +68,47 @@ def solution_dp(W, H, L, U, R, D):
     return res
 
 
+def solution_optimal(W, H, L, U, R, D):
+    from math import factorial
+    X = []
+    Y = []
+    res = 0
 
-solution = solution_dp
+    def get_x(i, j):
+        if not 1 <= i <= H or not 1 <= j <= W:
+            return
+        X.append([i, j])
+        get_x(i + 1, j - 1)
+
+    def get_y(i, j):
+        if not 1 <= i <= H or not 1 <= j <= W:
+            return
+        Y.append([i, j])
+        get_y(i - 1, j + 1)
+
+    get_x(D + 1, L - 1)
+    get_y(U - 1, R + 1)
+
+    for x in X:
+        x_prob = 0.5**(L + D - 2)
+        x_paths = factorial(x[0] + x[1] - 2) / (factorial(x[1] - 1) * factorial(x[0] - 1))
+        res += x_prob * x_paths
+
+    for y in Y:
+        y_prob = 0.5**(R + U - 2)
+        y_paths = factorial(y[0] + y[1] - 2) / (factorial(y[1] - 1) * factorial(y[0] - 1))
+        res += y_prob * y_paths
+
+    return res
+
+
+
+solution = solution_optimal
 
 tc = input()
-
 for i in range(1, int(tc) + 1):
     W, H, L, U, R, D = input().split()
-    # if i != 2:
+    # if i != 1:
     #     continue
     out = solution(int(W), int(H), int(L), int(U), int(R), int(D))
     print(f"Case #{i}: {out}")
